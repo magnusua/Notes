694955f2181d962c6e37e032f2be1ac980d56dad
Admin <uscmagnus@gmail.com>
2021-03-23 17:11:03 +0200

    alert2 разобраться почему не открывается фрагмент

---
+++ app/src/main/java/com/kugot/Fragment1.java
@@ -0,0 +1,41 @@
+package com.kugot;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * create an instance of this fragment.
+ */
+public class Fragment1 extends Fragment {
+
+    public Fragment1() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        setHasOptionsMenu(true);
+        return inflater.inflate(R.layout.fragment_1, container, false);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_menu, menu);
+    }
+}
--- app/src/main/java/com/kugot/MainActivity.java
+++ app/src/main/java/com/kugot/MainActivity.java
@@ -1,10 +1,18 @@
 package com.kugot;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.PopupMenu;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
-import com.kugot.ui.main.MainFragment;
+import com.google.android.material.button.MaterialButton;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,13 +20,98 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow();
-        }
-    }
-    protected void someMethod(){
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        MaterialButton btn;
+
+        btn = findViewById(R.id.btn_fragment1);
+        btn.setOnClickListener((view) -> {
+            f1();
+        });
+
+        btn = findViewById(R.id.btn_fragment2);
+        btn.setOnClickListener((view) -> {
+            f2();
+        });
+
+        btn = findViewById(R.id.btn_fragment3);
+        btn.setOnClickListener((view) -> {
+            f3();
+        });
+
+        final MaterialButton menuBtn = findViewById(R.id.btn_back);
+        menuBtn.setOnClickListener((view) -> {
+            PopupMenu popup = new PopupMenu(MainActivity.this, menuBtn);
+            popup.inflate(R.menu.button_menu);
+            popup.setOnMenuItemClickListener((item) -> {
+                FragmentManager manager = getSupportFragmentManager();
+                switch (item.getItemId()) {
+                    case R.id.menu_item_remove_fragment: {
+                        manager.popBackStack();
+                        return true;
+                    }
+                    case R.id.menu_item_remove_2_fragments: {
+                        manager.popBackStack();
+                        manager.popBackStack();
+                        return true;
+                    }
+                    default: {
+                        return false;
+                    }
+                }
+            });
+            popup.show();
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.toolbar_menu_item_f1: {
+                f1();
+                return true;
+            }
+            case R.id.toolbar_menu_item_f2: {
+                f2();
+                return true;
+            }
+            case R.id.toolbar_menu_item_f3: {
+                f3();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void f1() {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.fragment_container, new Fragment1());
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    private void f2() {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.fragment_container, new Fragment2());
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    private void f3() {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.fragment_container, new Fragment3());
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
